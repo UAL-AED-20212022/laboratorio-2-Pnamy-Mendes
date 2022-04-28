805e44bd27a2d56f76543886621e3cfea91755fc
Pedro Mendes <pedrito.kikas@gmail.com>
2022-04-28 18:09:38 +0100

    comments

--- task1/program.py
+++ task1/program.py
@@ -3,51 +3,48 @@ from sys import stdout
 stdout.reconfigure(encoding="utf-8")
 
 """
-    Este programa serve para a gestão de consultores e a gestão dos seus imóveis para venda.
+    Este programa é um Sistema de armazenamento de nomes de países, com o formato -> Linked List
     As suas funcionalidades são:
 
-        Consultores:
-            -> Registar Consultor. ['RC' + nome + apelido + NIF] (exemplo: <RC Maria Santos 123123333>) 
-                -> Sucesso: 'Consultor registado com sucesso.'
-                -> Insucesso: 
-                    -> Consultor já registado (verificação por NIF): 'Não é possível realizar o registo,consultor já registado.'
-                    -> Campos Insuficientes: 'Por favor verifique se todas as informações foram introduzidas.'
+        Países:
+            -> Registar País no inicio da lista. ['RPI' + nome_país_novo] (exemplo: <RPI Portugal>) 
+                -> Sucesso: É apresentada a lista de países atual depois de inserido o novo país.
+                
+            -> Registar País no fim da lista. ['RPF' + nome_país_novo] (exemplo: <RPI Brasil>) 
+                -> Sucesso: É apresentada a lista de países atual depois de inserido o novo país.
 
-            -> Eliminar Consultor. ['EC' + NIF] (exemplo: <EC 123123333> ) 
-                -> Sucesso: 
-                    'Consultor eliminado com sucesso.' 
-                    'Os imóveis <lista_ID_imoveis> também foram eliminados
-                        <lista_ID_imoveis> -> String de todos os Ids dos imoveis que pertenciam ao Consultor eliminado.
-                -> Insucesso: 
-                    -> NIF inválido: 'Não é possível realizar a operação,NIF inválido.'
-                    -> Campos Insuficientes: 'Por favor verifique se todas as informações foram introduzidas.'
+            -> Registar País Depois de outro Elemento já Registado. ['RPDE' + nome_país_novo + nome_país_registado] (exemplo: <RPI Chile Portugal>) 
+                -> Sucesso: É apresentada a lista de países atual depois de inserido o novo país.
 
-            Imóveis:
-                -> Registar Imóvel. ['RI' + NIF_consultor + localizacao_imovel + area_util + tipologia_imovel + ano_imovel + valor_venda_imovel + comissao_venda_imovel] 
-                    (exemplo: <RI 123123333 Porto 170 T4 2020 400000€ 0.20>)  
-                    -> Sucesso: 'Imóvel com <ID> registado com sucesso.' 
-                        <ID> -> Integer único gerado pelo programa.
-                    -> Insucesso: 
-                        -> NIF inválido: 'Não é possível realizar o registo do imóvel,NIF inválido.'
-                        -> Campos Insuficientes: 'Por favor verifique se todas as informações foram introduzidas.'
+            -> Registar País Antes de outro Elemento já Registado. ['RPAE' + nome_país_novo + nome_país_registado] (exemplo: <RPI Franca Portugal>) 
+                -> Sucesso: É apresentada a lista de países atual depois de inserido o novo país.
 
-                -> Listar imóveis dum consultor. ['LI' + NIF] (exemplo: <LI 123123333>)
-                    -> Sucesso: 
-                        -> Com imóveis registados: '<ID_imovel> <valor_venda_imovel> <comissao_venda_imovel>.' para cada imóvel pertencente ao consultor.
-                            <ID_imovel> -> Integer único gerado pelo programa que repreenta o imóvel.
-                            <valor_venda_imovel> -> Valor do Imóvel em euros (€).
-                            <comissao_venda_imovel> -> Comissão de venda do Imóvel.
-                        -> Sem imóveis registados: 'Não existem imóveis registados.'
-                    -> Insucesso: 
-                        -> NIF inválido: 'Não é possível realizar o registo do imóvel,NIF inválido'
-                        -> Campos Insuficientes: 'Por favor verifique se todas as informações foram introduzidas.'
+            -> Registar País num determinado Índice. ['RPII' + nome_país_novo + indice] (exemplo: <RPI Italia 1>) 
+                -> Sucesso: É apresentada a lista de países atual depois de inserido o novo país.
 
-                -> Eliminar Imóvel. ['EI' + NIF + ID_imóvel] (exemplo: <EI 123123333 1>)
-                    -> Sucesso: 'Imóvel eliminado com sucesso.'
-                    -> Insucesso: 
-                        -> Imóvel não registado: 'O ID indicado n«é inválido.'
-                        -> Imóvel não pertence ao NIF indicado: 'O imóvel <ID_imovel> não pertence ao consultor indicado.'
-                        -> Campos Insuficientes: 'Por favor verifique se todas as informações foram introduzidas.'
+            -> Verificar Número de elementos da lista. ['VNE'] (exemplo: <VNE>) 
+                -> Sucesso: O número de elementos são <número_elementos>.
+
+            -> Verificar se um País se encontra na lista. ['VP' + nome_pais] (exemplo: <VP Portugal>) 
+                -> Sucesso: O país <nome_país> encontra-se na lista.
+                -> Insucesso: O país <nome_país> não se encontra na lista.
+
+            -> Eliminar o primeiro elemento da lista. ['EPE'] (exemplo: <EPE>) 
+                -> Sucesso: O país <nome_país_eliminado> foi eliminado da lista.
+
+            -> Eliminar o último elemento da lista. ['EUE'] (exemplo: <EUE>) 
+                -> Sucesso: O país <nome_país_eliminado> foi eliminado da lista.
+
+            -> Eliminar um determinado país da lista. ['EP' + nome_país] (exemplo: <EP Portugal>) 
+                -> Sucesso: O país <nome_país_eliminado> foi eliminado da lista.
+                -> Insucesso: O país <nome_país> não se encontra na lista.
+
+            
+    Os requerimento do programa são:
+        Nenhum.
+
+    
+    Nenhum módulo extra é usado, sendo unicamente necessário o Python v3.10 para executar o prgrama.
 """
 
 def main() -> None:
